aa5dd8d9163ce675a1a5c2e8d0546606ae436247
Charlotte Fusco <ch.fusco104@gmail.com>
2023-03-22 19:03:47 -0500

    CRUD for bookmarks, allow null value for bookmark questionId

--- controllers/BookmarkController.js
+++ controllers/BookmarkController.js
@@ -1,5 +1,20 @@
 const { Bookmark } = require('../models')
 
+//C
+const createBookmark = async (req, res) => {
+  try {
+    let ownerId = parseInt(req.params.userId)
+    let bookmarkContent = {
+      ownerId,
+      ...req.body
+    }
+    let bookmark = Bookmark.create(bookmarkContent)
+    res.send(bookmark)
+  } catch (error) {
+    throw error
+  }
+}
+
 //R
 const getBookmarksAll = async (req, res) => {
   try {
@@ -10,6 +25,21 @@ const getBookmarksAll = async (req, res) => {
   }
 }
 
+//D
+const deleteBookmark = async (req, res) => {
+  try {
+    let bookmarkId = parseInt(req.body.bookmarkId)
+    await Bookmark.destroy({
+      where: { id: bookmarkId }
+    })
+    res.send(`Successfully deleted bookmark id of ${bookmarkId}.`)
+  } catch (error) {
+    throw error
+  }
+}
+
 module.exports = {
-  getBookmarksAll
+  createBookmark,
+  getBookmarksAll,
+  deleteBookmark
 }

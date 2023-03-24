8429d2e3acc033b68bfd79f3ef7a77d9e1cc215d
Austin <austinih46@gmail.com>
2023-03-24 08:53:04 -0500

    Updated ERD on Backend ReadMe

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -4,7 +4,9 @@ const controller = require('../controllers/UserController')
 Router.get('/view', controller.getUsersAll)
 Router.get('/view/:email', controller.getUserByEmail)
 Router.put('/:email', controller.updateUser)
+Router.put('/update/:id', controller.updateUserById)
 Router.delete('/:email', controller.deleteUser)
+Router.delete('/delete/:id', controller.deleteUserById)
 
 
 module.exports = Router
